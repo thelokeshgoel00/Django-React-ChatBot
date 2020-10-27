b306f5a2e2fd5a50e4e91fa1b774b8d9057d34d8
InsaneNerd <40207268+thelokeshgoel00@users.noreply.github.com>
2020-10-27 12:22:01 +0530

    Update settings.py

--- chatSite/chatSite/settings.py
+++ chatSite/chatSite/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mx@!x1r7$i4z50607p3yvt@_a-3gdkd^1i8g%_#rd#u3h)dgi&'
-
+SECRET_KEY = 'Your-Secret-Api-Key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
